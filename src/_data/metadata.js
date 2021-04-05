4f0ee5cfabac08eb30e6a8bda07faf60604511d7
Dmitrii Kuznetsov <dk@bayda.ru>
2021-04-05 18:07:38 +0100

    fix metadata

---
+++ src/_data/metadata.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'Frontendj',
+    url: 'http://frontendj.ru',
+    language: 'en',
+    description: 'Personal website of Mitya Kuznetsov - a front end developer and a swing dj',
+    ogImage: 'http://frontendj.ru/images/og.jpg',
+    feed: {
+        subtitle: 'I am writing about my experience with UI and (maybe) other things in my life',
+        filename: 'feed.xml',
+        path: '/feed/rss.xml',
+        id: 'http://frontendj.ru/'
+    }
+};
