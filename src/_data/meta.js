12f8556f761846e2c79fcaffc15746e1e4328e43
Dmitrii Kuznetsov <dk@bayda.ru>
2021-04-05 17:26:48 +0100

    add open graph

---
+++ src/_data/meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: 'Frontendj',
+    author: 'Mitya Kuznetsov',
+    description: 'Personal website of Mitya Kuznetsov - a front end developer and a swing dj',
+    paths: {
+        site: 'https://frontendj.ru',
+        ogImage: 'https://frontendj.ru/images/og.png',
+    }
+};
